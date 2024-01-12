46121a8afbc0df4d80dac5a6d533f533ba6f5f16
Andrii <andrii.khomik@gmail.com>
2024-01-12 15:12:07 +0200

    add 404 page

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,16 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/board", boardRoute);
 app.use("/card", cardRoute);
 
+app.all("*", (req, res) => {
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
