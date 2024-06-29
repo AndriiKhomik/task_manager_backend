d0c0201ad6aabfcdb677171f09428f3768667489
Andrii <andrii.khomik@gmail.com>
2024-06-29 13:22:19 +0300

    change url

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 export const dbConnect = async () => {
   mongoose.set("strictQuery", true);
   try {
-    await mongoose.connect(process.env.DATABASE_URI!);
-    // await mongoose.connect(
-    //   `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.yjwj9pa.mongodb.net/?retryWrites=true&w=majority`
-    // );
+    // await mongoose.connect(process.env.DATABASE_URI!);
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.yjwj9pa.mongodb.net/?retryWrites=true&w=majority`
+    );
   } catch (err) {
     console.log(err);
   }
