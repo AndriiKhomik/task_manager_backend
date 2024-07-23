4bea404c40a0a18701aa0b480515c0bd2adbbd4f
Andrii <andrii.khomik@gmail.com>
2024-07-23 19:35:21 +0300

    switch to default cors

--- src/controllers/boardController.ts
+++ src/controllers/boardController.ts
@@ -9,7 +9,6 @@ export const getBoards = async (
   next: NextFunction
 ) => {
   const boards = await Board.find();
-  console.log(boards);
 
   if (!boards) {
     return res.status(400).json({ message: "Boards not found" });
--- src/server.ts
+++ src/server.ts
@@ -16,22 +16,22 @@ const app = express();
 
 dbConnect();
 
-const allowedOrigins = [
-  "*",
-  "https://task-manager-fv49.onrender.com",
-  "http://localhost:3000",
-  "http://localhost:80",
-];
+// const allowedOrigins = [
+//   "*",
+//   "https://task-manager-fv49.onrender.com",
+//   "http://localhost:3000",
+//   "http://localhost:80",
+// ];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+//   })
+// );
 
-// app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
