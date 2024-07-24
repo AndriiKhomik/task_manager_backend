a0f810603798b53b2169a3bda4c6a894f2e5756f
Andrii <andrii.khomik@gmail.com>
2024-07-24 19:28:14 +0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -16,26 +16,26 @@ const app = express();
 
 dbConnect();
 
-// const allowedOrigins = [
-//   "*",
-//   "https://task-manager-fv49.onrender.com",
-//   "http://localhost:3000",
-//   "http://localhost:80",
-// ];
-
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     credentials: true,
-//   })
-// );
+const allowedOrigins = [
+  "*",
+  "https://task-manager-fv49.onrender.com",
+  "http://localhost:3000",
+  "http://http://192.168.56.101",
+];
 
 app.use(
   cors({
-    origin: "*",
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 app.use(express.json());
 
